698bcb03a2e48a421f7b036dfbc714723f448f21
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-09-05 10:45:27 +0500

    Update question2.c

--- Labs/lab03/question2.c
+++ Labs/lab03/question2.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main() {
+         int len=0;
+         int width=0;
+         int depth=0;
+         printf("== Find Volume ==\n");
+         printf("--Taking Inputd--\n");
+         printf("Enter Length:  ");
+         scanf("%d",&len);
+         printf("Enter Width:  ");
+         scanf("%d",&width);
+         printf("Enter Depth:  ");
+         scanf("%d",&depth);
+         
+         printf("\n--DONE...Taking Inputs--\n");
+         printf("\n--Calculating Volume--\n");
+ 
+        int volume = len * width * depth;
+        printf("Volume Is: %d",volume);
+        printf("");
+        return 1;
+}//end main
+         
