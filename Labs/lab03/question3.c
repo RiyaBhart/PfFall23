1ec6f56a34467dc5014fa4c1801cfe76d73ff9de
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-09-05 20:50:38 +0500

    Create question3.c

---
+++ Labs/lab03/question3.c
@@ -0,0 +1,20 @@
+int main() {
+    int base = 0;
+    int height = 0;
+    printf("== Calculating Hypotenuse ==\n");
+    printf("--Taking Inputs--\n");
+    printf(" Enter base:  ");
+    scanf("%d",&base);
+    printf("Enter height:  ");
+    scanf("%d",&height);
+    
+    printf("\n--Calculating Hypotenuse--\n");
+    int hypotenuse = base*base + height*height; 
+    float h =sqrt(hypotenuse);
+    
+    printf("hypotenuse is: %f" ,h);
+
+    
+
+    return 0;
+}
