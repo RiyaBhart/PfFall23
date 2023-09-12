ab57e9fc80b141064fec4f6cf9dc5aecd77a038e
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-09-12 10:18:57 +0500

    Create q02.c

---
+++ Labs/04/q02.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+int main() {
+         int n=0;
+         int a=0;
+         int m=0;
+        
+         printf("Enter n:  ");
+         scanf("%d", &n);
+
+         for(a=1;a<=n;a++) {
+         for(m=1; m<=n;m++) {
+         printf("*"); 
+}
+         printf("\n");
+}
+return 0;
+}
