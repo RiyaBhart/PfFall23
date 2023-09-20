fd5b15bd265ef714979cc0c10672ac29571949f9
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-09-20 23:05:50 +0500

    Update question09.c

--- Labs/05/question09.c
+++ Labs/05/question09.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 void main() {
     int a,b,c,disc;
@@ -7,21 +8,29 @@ void main() {
     printf("Enter b (co. efficient of x):   ");
     scanf("%d",&b);
     printf("Enter Constant:   ");
-    scanf("%d",&c);
-    disc= b*b - (4*a*c);
+    scanf("%d",&c);disc= b*b - (4*a*c);
     if(disc>0){
-        printf("Roots are real",disc);
+        printf("Roots are real %d\n",disc);
+	    float y1= (-1*b + sqrt((b*b) - 4*a*c))/(2*a);
+    	float y2= (-1*b - sqrt((b*b) - 4*a*c))/(2*a);
+    
+    	printf("y1 is: %f\n" ,y1);
+    	printf("y2 is: %f\n", y2);
+    
     }
     else if(disc<0){
-        printf("Roots are imaginary",disc);
+        printf("Roots are imaginary %d",disc);
+    
     }
     else{//disc==0
+        printf("Roots are Equal %d",disc);
+        float y1= (-1*b + sqrt((b*b) - 4*a*c))/(2*a);
+    	float y2= (-1*b - sqrt((b*b) - 4*a*c))/(2*a);
     
-        printf("Roots are Equal",disc);
-    }
-    
-    
-    
-    return 0;
+    	printf("y1 is: %f\n" ,y1);
+    	printf("y2 is: %f\n", y2); 
+   
 
+}
+    return 0;
 }
