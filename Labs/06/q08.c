0ba6d0bd735be5564d9105851cd9be27c6c902b3
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-10-06 00:25:43 +0500

    Create q08.c

---
+++ Labs/06/q08.c
@@ -0,0 +1,12 @@
+/** 
+*Programmer : Riya Bhart
+*Desc: Lab06; sum using arrays
+*Date: 10/3/23
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    printf("%d,%d,%d,%d,%d,%d,%d,%d,%d",arr[8],arr[7],arr[6],arr[5],arr[4],arr[3],arr[2],arr[1],arr[0]);
+    return 0;
+}
