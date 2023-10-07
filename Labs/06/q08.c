ac3244cb162dd0f51de84530af0efb0f8fae2a57
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-10-08 03:28:45 +0500

    Update q08.c

--- Labs/06/q08.c
+++ Labs/06/q08.c
@@ -6,7 +6,16 @@
 #include <stdio.h>
 
 int main() {
-    int arr[]={1,2,3,4,5,6,7,8,9};
-    printf("%d,%d,%d,%d,%d,%d,%d,%d,%d",arr[8],arr[7],arr[6],arr[5],arr[4],arr[3],arr[2],arr[1],arr[0]);
+    int n;
+    printf("Enter number of values in the array:  ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("input the value in array:  ");
+        scanf("%d",&arr[i]);
+    }
+    for(int i=n-1;i>=0;i--){
+        printf("%d\n",arr[i]);
+    }
     return 0;
 }
