c44ca7ae7c955c5c63172dd1244931b9031e3e3c
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-10-31 22:52:41 +0500

    Create q01.c

---
+++ Labs/09/q01.c
@@ -0,0 +1,22 @@
+/** 
+*Programmer : Riya Bhart
+*Desc: Lab09;multiplication of two integers using recursion 
+*Date: 31/10/23
+*/
+#include <stdio.h>
+int factorial(int j){
+	if(j==1|| j==0){
+		return 1;
+	}
+	else{
+	    return j*(factorial(j-1));
+	}
+}
+int main(){
+	int n;
+	printf("enter number: ");
+	scanf("%d",&n);
+	int x = factorial(n);
+	printf("%d",x);
+	return 0;
+}
