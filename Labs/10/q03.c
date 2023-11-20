454c6bb3e86492bc64efd06fef92bdfd9888c9f1
RiyaBhart <142867648+RiyaBhart@users.noreply.github.com>
2023-11-20 21:25:48 +0500

    Create q03.c

---
+++ Labs/10/q03.c
@@ -0,0 +1,58 @@
+/*
+ * Programmer: Riya Bhart
+ * Date: 19/11/23
+ * Description: dynamically create a 2d array and initialize it with prime numbers
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_prime(int n)
+{
+	// check if prime number
+	for (int i = 2; i*i <= n; i++) 
+	if (!(n % i))
+	return 0;
+	return 1;
+} // end is_prime()
+
+void show_arr(int** arr, int x, int y)
+{
+	for (int i = 0; i < y; i++) {
+		for (int j = 0; j < x; j++)
+		printf("%-4d", arr[i][j]);
+		printf("\n");
+	}
+}
+
+int **create_arr(int x, int y)
+{
+	int **arr = (int **)malloc(sizeof(int *) * y);
+	for (int i = 0; i < y; i++) 
+	arr[i] = (int *)malloc(sizeof(int) * x);
+
+	for (int i = 0, num = 2; i < y; i++) {
+		for (int j = 0; j < x; j++) {
+			for(;!is_prime(num);num++);
+			arr[i][j] = num++;
+		}
+	}
+
+    return arr;
+}
+
+void free_arr(int **arr, int y)
+{
+	
+	for (int i = 0; i < y; i++) free(arr[i]);
+	free(arr);
+} 
+
+int main()
+{
+	int x = 5, y = 5, ** arr = create_arr(x, y);
+	show_arr(arr, x, y);
+	free_arr(arr, y);
+
+	return 0;
+} 
